4c8eef2aa885fa5ad45a045670159156cf3affbd
adityaSS13 <aditya.shahapuremit@gmail.com>
2025-04-14 13:30:10 -0400

    Fix amplify routing

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
+  base: "/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
